4deb722af2416a115327cec8505cf94f78e8b117
texonth <texon.th@gmail.com>
2019-06-12 18:26:53 +0300

    load beta of new version

---
+++ precache-manifest.a58e4dc3c579eff3a667cafdb7ed3d8a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62559cd8c620b5bc3d34b30ea57ecc41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98ae319163c2f097a0e5",
+    "url": "/static/css/main.90427816.chunk.css"
+  },
+  {
+    "revision": "4938498c86d70601fa46",
+    "url": "/static/js/2.74ddfac0.chunk.js"
+  },
+  {
+    "revision": "98ae319163c2f097a0e5",
+    "url": "/static/js/main.afec7605.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14c86d98d8c7539490db4fa4078e9970",
+    "url": "/static/media/css.14c86d98.svg"
+  },
+  {
+    "revision": "1e7beba60cd0097a79e3fb6bf49c5c6e",
+    "url": "/static/media/git.1e7beba6.svg"
+  },
+  {
+    "revision": "79fcb3bc1f77f5f402ff028af3120a2d",
+    "url": "/static/media/html.79fcb3bc.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/static/media/js.dd60b11e.svg"
+  },
+  {
+    "revision": "ae584155fa83698b59dd9957f1a82342",
+    "url": "/static/media/mongodb.ae584155.svg"
+  },
+  {
+    "revision": "c2c46982ab55267699dc40ee686269bf",
+    "url": "/static/media/nodejs.c2c46982.svg"
+  },
+  {
+    "revision": "589d1289739b4eea81316c15b52447ce",
+    "url": "/static/media/photo.589d1289.jpg"
+  },
+  {
+    "revision": "ad03ab0463b04455e622692fe37e6ebc",
+    "url": "/static/media/react.ad03ab04.svg"
+  },
+  {
+    "revision": "4e231cdcd36fa633df03777efebfc26f",
+    "url": "/static/media/redux.4e231cdc.svg"
+  },
+  {
+    "revision": "242dcb9396faebe6d0a27e040f345fbc",
+    "url": "/static/media/remote.242dcb93.svg"
+  },
+  {
+    "revision": "2ee7b9867d7180f0d412b29e0b284504",
+    "url": "/static/media/sass.2ee7b986.svg"
+  },
+  {
+    "revision": "c6b325e500152d080454f75bb2e8dd41",
+    "url": "/static/media/sorts.c6b325e5.png"
+  },
+  {
+    "revision": "f7bde6a30c631d28d8bf1e015cb6c9f3",
+    "url": "/static/media/svg.f7bde6a3.svg"
+  },
+  {
+    "revision": "c6ef19cbcba4722d8f4c6aed60bb14b9",
+    "url": "/static/media/vue.c6ef19cb.svg"
+  },
+  {
+    "revision": "bd650f62feb2e8cb25f554787cee4f8d",
+    "url": "/static/media/webpack.bd650f62.svg"
+  },
+  {
+    "revision": "64b2dfa76e2a66b22a8eb8281b9658ac",
+    "url": "/static/media/workplace.64b2dfa7.svg"
+  },
+  {
+    "revision": "62057a2dd2ed7174571a04bf0f6973b7",
+    "url": "/static/media/zzz.62057a2d.svg"
+  }
+]);
